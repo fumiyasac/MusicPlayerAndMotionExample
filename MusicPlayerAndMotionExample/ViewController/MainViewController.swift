04e43c7ada819f371946516188c26e88e8e3e096
Fumiya Sakai <just1factory@gmail.com>
2021-09-07 23:47:36 +0900

    Initial Commit

---
+++ MusicPlayerAndMotionExample/ViewController/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  MusicPlayerAndMotionExample
+//
+//  Created by 酒井文也 on 2021/09/02.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
