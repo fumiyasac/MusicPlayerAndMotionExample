fbc574840f77a8d82fdfbb30a12476fab5b0da0f
Fumiya Sakai <just1factory@gmail.com>
2021-09-20 22:46:02 +0900

    SwiftUI用のViewを新規追加

---
+++ MusicPlayerAndMotionExample/SwiftUI/Screen/MainScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenView.swift
+//  MusicPlayerAndMotionExample
+//
+//  Created by 酒井文也 on 2021/09/20.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreenView()
+    }
+}
